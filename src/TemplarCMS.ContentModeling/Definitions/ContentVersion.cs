1815b06fef72e5337b74f1faf85d5834b2b5e38a
Brett Murphy <murphy.g.brett@gmail.com>
2026-06-09 18:58:44 -0400

    Add content version value object

---
+++ src/TemplarCMS.ContentModeling/Definitions/ContentVersion.cs
@@ -0,0 +1,42 @@
+namespace TemplarCMS.ContentModeling.Definitions;
+
+/// <summary>
+/// Represents a content version number.
+/// </summary>
+/// <param name="Value">The version value.</param>
+public readonly record struct ContentVersion(int Value)
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ContentVersion" /> struct.
+    /// </summary>
+    /// <param name="value">The version value.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the version value is negative.</exception>
+    public ContentVersion(int value)
+        : this()
+    {
+        if (value < 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(value),
+                "Content version cannot be negative.");
+        }
+
+        Value = value;
+    }
+
+    /// <summary>
+    /// Gets the shared value version.
+    /// </summary>
+    public static ContentVersion Shared => new(0);
+
+    /// <summary>
+    /// Gets the first authored content version.
+    /// </summary>
+    public static ContentVersion First => new(1);
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+}
