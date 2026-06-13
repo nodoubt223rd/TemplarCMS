4ecb59f144bf1fe417bc952ce2ad2159124103b9
Brett Murphy <murphy.g.brett@gmail.com>
2026-06-13 15:29:08 -0400

    Add exact match field value resolution policy tests

---
+++ tests/TemplarCMS.ContentModeling.Tests/Definitions/ExactMatchFieldValueResolutionPolicyTests.cs
@@ -0,0 +1,83 @@
+using TemplarCMS.ContentModeling.Definitions;
+using Xunit;
+
+namespace TemplarCMS.ContentModeling.Tests.Definitions;
+
+public sealed class ExactMatchFieldValueResolutionPolicyTests
+{
+    private readonly ExactMatchFieldValueResolutionPolicy _policy = new();
+
+    [Fact]
+    public void Resolve_Throws_WhenFieldDefinitionIsNull()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+            _policy.Resolve(null!, [], new ContentLanguage("en"), ContentVersion.First));
+    }
+
+    [Fact]
+    public void Resolve_Throws_WhenValuesIsNull()
+    {
+        var field = CreateVersionedField();
+
+        Assert.Throws<ArgumentNullException>(() =>
+            _policy.Resolve(field, null!, new ContentLanguage("en"), ContentVersion.First));
+    }
+
+    [Fact]
+    public void Resolve_ReturnsSharedValue()
+    {
+        var field = CreateSharedField();
+        var expected = CreateValue(new ContentLanguage("en"), ContentVersion.Shared);
+
+        var result = _policy.Resolve(field, [expected], new ContentLanguage("fr"), ContentVersion.First);
+
+        Assert.Same(expected, result);
+    }
+
+    [Fact]
+    public void Resolve_ReturnsUnversionedValue()
+    {
+        var field = CreateUnversionedField();
+        var expected = CreateValue(new ContentLanguage("en"), ContentVersion.Shared);
+
+        var result = _policy.Resolve(field, [expected], new ContentLanguage("en"), ContentVersion.First);
+
+        Assert.Same(expected, result);
+    }
+
+    [Fact]
+    public void Resolve_ReturnsVersionedValue()
+    {
+        var field = CreateVersionedField();
+        var expected = CreateValue(new ContentLanguage("en"), new ContentVersion(2));
+
+        var result = _policy.Resolve(field, [expected], new ContentLanguage("en"), new ContentVersion(2));
+
+        Assert.Same(expected, result);
+    }
+
+    [Fact]
+    public void Resolve_ReturnsNull_WhenNoMatchExists()
+    {
+        var field = CreateVersionedField();
+        var value = CreateValue(new ContentLanguage("en"), ContentVersion.First);
+
+        var result = _policy.Resolve(field, [value], new ContentLanguage("fr"), ContentVersion.First);
+
+        Assert.Null(result);
+    }
+
+    private static ContentFieldValue CreateValue(ContentLanguage language, ContentVersion version)
+    {
+        return new ContentFieldValue(Guid.NewGuid(), Guid.NewGuid(), "title", language, version, "Home");
+    }
+
+    private static FieldDefinition CreateSharedField() =>
+        new(Guid.NewGuid(), "Title", "title", FieldType.SingleLineText, isShared: true);
+
+    private static FieldDefinition CreateUnversionedField() =>
+        new(Guid.NewGuid(), "Title", "title", FieldType.SingleLineText, isUnversioned: true);
+
+    private static FieldDefinition CreateVersionedField() =>
+        new(Guid.NewGuid(), "Title", "title", FieldType.SingleLineText);
+}
