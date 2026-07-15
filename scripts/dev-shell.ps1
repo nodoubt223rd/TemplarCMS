cc2f9c336fcc0422a8f036a8c9661c56487d43d4
Brett Murphy <murphy.g.brett@gmail.com>
2026-07-15 12:13:44 -0400

    Align SDK and restore paths

--- scripts/dev-shell.ps1
+++ scripts/dev-shell.ps1
@@ -3,5 +3,6 @@
 Write-Host 'TemplarCMS dev shell configured:'
 Write-Host "  DOTNET_CLI_HOME=$env:DOTNET_CLI_HOME"
 Write-Host "  NUGET_PACKAGES=$env:NUGET_PACKAGES"
+Write-Host "  APPDATA=$env:APPDATA"
 Write-Host "  DOTNET_SKIP_FIRST_TIME_EXPERIENCE=$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE"
 Write-Host "  DOTNET_NOLOGO=$env:DOTNET_NOLOGO"
